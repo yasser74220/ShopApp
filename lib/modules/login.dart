97b8904046be595b0fc4a59e05c9c8c6b9650205
yasser74220 <yasserahlawy970@gmail.com>
2022-08-25 22:53:01 +0200

    Login

--- lib/modules/login.dart
+++ lib/modules/login.dart
@@ -1,12 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/shared/components/components.dart';
 
 class ShopLogin extends StatelessWidget {
-  const ShopLogin({Key? key}) : super(key: key);
+   var EmailController = TextEditingController();
+   var PasswordController = TextEditingController();
 
-  @override
+   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Text(
+              'LOGIN',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            Text(
+              'login now to browse our hot offers',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyText1
+                  ?.copyWith(color: Colors.grey),
+            ),
+            defualtInput(
+              controller: EmailController,
+              inputType: TextInputType.emailAddress,
+              validate: (value) {
+                
+              },
+              onTap: () {
+                
+              },
+              iconPre:  Icons.email,
+
+            ),
+            defualtInput(
+              controller: PasswordController,
+              inputType: TextInputType.visiblePassword,
+              validate: (value) {
+
+              },
+              onTap: () {
+
+              },
+              iconPre:  Icons.password,
+              isPassword: true,
+              issuffix: true,
+              iconSuff: Icons.remove_red_eye,
+
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
